7ebfdd3f3ef4ccf5737d440eee7fdc9ff07a95dd
KirillNowoselow <98112009+KirillNowoselow@users.noreply.github.com>
2022-01-23 19:42:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ if __name__ == "__main__":
   b = int(input('Введите коеффициент b: '))
   c = int(input('Введите коеффициент c: '))
   D = b**2-4*a*c
-  x1 = (-b+D**(1/2))/(2*a)
-  x2 = (-b-D**(1/2))/(2*a)
-  if D>=0:
-    if D==0:
-      print('X= ', x1)
-    else:
-      print('X1= ', x1, 'X2= ', x2)
+  if D>0:
+    x1 = (-b+D**(1/2))/(2*a)
+    x2 = (-b-D**(1/2))/(2*a)
+    print('X1= ', x1, 'X2= ', x2)
+  elif D==0:
+    x = (-b)/(2*a)
+    print('X= ', x)
   else:
     print('Нет корней')
